aad4162b38fcbb79ff390ef7af45863ddd08c5b8
Sars <sars@S-MacBook-Pro.local>
2016-07-14 12:30:04 +0800

    Add some softwares into Brew Cask.

--- macOS/BrewCask.sh
+++ macOS/BrewCask.sh
@@ -14,14 +14,21 @@ brew cask install caskroom-versions/sublime-text3
 brew cask install dropbox
 brew cask install evernote
 brew cask install firefox
+brew cask install franz
 brew cask install google-chrome
+brew cask install google-drive
 brew cask install mou
 brew cask install mplayerx
 brew cask install nally
+brew cask install quip
+brew cask install sketch
 brew cask install skype
+brew cask install slack
 brew cask install sourcetree
 brew cask install spotify
+brew cask install telegram-desktop
 brew cask install virtualbox
+brew cask install wireshark
 
 # https://github.com/sindresorhus/quick-look-plugins
 brew cask install qlimagesize
