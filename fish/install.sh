e7b88d2f54cc31334e8c094d131e05c9531267a5
Sars <sars.tw@gmail.com>
2018-03-26 16:21:45 +0800

    Do not send LANG for ssh client

--- fish/install.sh
+++ fish/install.sh
@@ -6,5 +6,8 @@ mkdir -p ~/.config/fish/conf.d/
 cp config.fish ~/.config/fish/
 cp fish_command_timer.fish ~/.config/fish/conf.d/
 
+# Don't send LANG for ssh client
+sed -i '.bak' 's/SendEnv LANG LC_/#SendEnv LANG LC_/' ssh_config
+
 chsh -s /usr/local/bin/fish
 
