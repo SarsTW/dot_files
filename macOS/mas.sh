9046ff8a8debd9aab797dc3e394aba7f7118dd6c
Sars Chang <sars.tw@gmail.com>
2018-08-25 19:46:34 +0800

    Add mas file

---
+++ macOS/mas.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mas install 494803304
+mas install 1105332179
+mas install 497799835
+mas install 409183694
+mas install 417602904
+mas install 836500024
+mas install 485812721
+mas install 595191960
+mas install 410628904
+mas install 585829637
+mas install 409201541
+mas install 1287445660
+mas install 539883307
+mas install 1176895641
+mas install 1356043814
+mas install 1127253508
+mas install 409203825
+
