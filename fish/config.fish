4014a73a10cc010b15a6308ce44d088448024ce5
Sars Chang <sars.tw@gmail.com>
2018-11-24 03:27:57 +0800

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,9 @@
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 
-source ~/.nvm-fish/nvm.fish
+mkdir -p ~/.nvm
 
-export NVM_DIR="$HOME/.nvm"
+bass export NVM_DIR="$HOME/.nvm"
 bass . "/usr/local/opt/nvm/nvm.sh"
-
-# The next line updates PATH for the Google Cloud SDK.
-bass source '/Users/sars/Developer/google-cloud-sdk/path.bash.inc'
-
-# The next line enables shell command completion for gcloud.
-bass source '/Users/sars/Developer/google-cloud-sdk/completion.bash.inc'
 
 export GOPATH=$HOME/go/
 
