d581e187fef7120e232573e061790c16ba3ed7b2
Sars Chang <sars.tw@gmail.com>
2018-11-24 03:29:05 +0800

    Update fish install script

--- fish/install.sh
+++ fish/install.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
 
-sudo echo /usr/local/bin/fish >> /etc/shells
+git clone https://github.com/edc/bass.git
+cd bass && make install && cd ..
+
+echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
+
 
 mkdir -p ~/.config/fish/conf.d/
 cp config.fish ~/.config/fish/
 cp fish_command_timer.fish ~/.config/fish/conf.d/
 
 # Don't send LANG for ssh client
-sed -i '.bak' 's/SendEnv LANG LC_/#SendEnv LANG LC_/' ssh_config
+sudo sed -i '.bak' 's/SendEnv LANG LC_/#SendEnv LANG LC_/' /etc/ssh/ssh_config
 
 chsh -s /usr/local/bin/fish
+
+
+# Install nvm in fish shell. https://github.com/brigand/fast-nvm-fish
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+curl -sSL https://raw.githubusercontent.com/brigand/fast-nvm-fish/master/nvm.fish > ~/.config/fish/functions/nvm.fish
+
+nvm install v8 && nvm use v8
 
