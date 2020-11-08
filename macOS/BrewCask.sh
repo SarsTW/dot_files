b548b8b9896a671cd4e4f9eeb2ebbfcdf58bb7fe
Sars Chang <sars.tw@gmail.com>
2020-11-08 19:41:38 +0800

    Add git config ignoreCase to false. Add tableplus in brew cask

--- macOS/BrewCask.sh
+++ macOS/BrewCask.sh
@@ -44,6 +44,7 @@ brew cask install slack
 brew cask install sourcetree
 brew cask install spotify
 brew cask install sublime-text
+brew cask install tableplus
 brew cask install telegram-desktop
 brew cask install thunderbird
 brew cask install typora
