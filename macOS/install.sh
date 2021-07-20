19e9bc30786db7ca012b72634b73f39c230bd01f
Sars Chang <sars.tw@gmail.com>
2021-07-20 08:15:28 +0800

    Fix Thunderbird Customize style

--- macOS/install.sh
+++ macOS/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-cd ~/Library/Thunderbird/Profiles/*.default/
+cd ~/Library/Thunderbird/Profiles/*.default-release/
+
+sed '/toolkit.legacyUserProfileCustomizations.stylesheets/ s/false/true/' prefs.js > prefs.js
 
 mkdir chrome
 
 cd -
 
-cp thunderbird/userChrome.css ~/Library/Thunderbird/Profiles/*.default/chrome/
+cp thunderbird/userChrome.css ~/Library/Thunderbird/Profiles/*.default-release/chrome/
 
