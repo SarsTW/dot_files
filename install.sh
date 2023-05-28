a11b05b44f5b0c0fd3c2eb98bc95061d05920274
Sars Chang <sars.tw@gmail.com>
2023-05-28 12:46:13 +0800

    Update gpg

--- install.sh
+++ install.sh
@@ -6,3 +6,10 @@ cp gitignore_global ~/.gitignore_global
 cp screenrc ~/.screenrc
 cp vimrc ~/.vimrc
 
+mkdir ~/.gnupg
+chmod 700 ~/.gnupg
+echo "pinentry-program $(brew --prefix)/bin/pinentry-mac" > ~/.gnupg/gpg-agent.conf
+echo 'use-agent' > ~/.gnupg/gpg.conf
+git config --global gpg.program $(which gpg)
+git config --global user.signingkey C4E4A40E
+
